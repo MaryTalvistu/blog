10282a0a1fcf22f4ac60a91ec13613581510730c
Mary Talvistu <marytalvistu@gmail.com>
2021-01-20 14:41:48 +0200

    Poolik registreerimise vorm

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -7,15 +7,15 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item">
-                    <a href="<?php echo URLROOT;?>" class="nav-link">Home</a>
+                    <a href="<?php echo URLROOT; ?>" class="nav-link">Home</a>
                 </li>
             </ul>
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
-                    <a href="<?php echo URLROOT;?>/users/register" class="nav-link">Register</a>
+                    <a href="<?php echo URLROOT; ?>/users/register" class="nav-link">Register</a>
                 </li>
                 <li class="nav-item">
-                    <a href="<?php echo URLROOT;?>/users/login" class="nav-link">Login</a>
+                    <a href="<?php echo URLROOT; ?>/users/login" class="nav-link">Login</a>
                 </li>
             </ul>
         </div>
